71b6ef18d04ce612ecf9b695535eb5f94e505ebf
Chao-Wei Chen <willhyper@gmail.com>
2020-09-06 23:47:31 -0700

    + :compiler-options {:optimizations :advanced} - [reagent "0.8.0-alpha2"]

--- src/raycasting/core.cljs
+++ src/raycasting/core.cljs
@@ -1,5 +1,4 @@
-(ns raycasting.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+(ns raycasting.core)
 
 ;; define your app data so that it doesn't get over-written on reload
 
@@ -10,9 +9,7 @@
    [:h1 (:text @app-state)]
    [:h3 "Edit this and watch it change!"]])
 
-(defn start []
-  (reagent/render-component [hello-world]
-                            (. js/document (getElementById "app"))))
+(defn start [])
 
 (defn ^:export init []
   ;; init is called ONCE when the page loads
