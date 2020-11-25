fb0d8579eea9c2973ef87d7fed58e2db47818709
Chao-Wei Chen <willhyper@gmail.com>
2020-11-24 20:25:49 -0800

    comment

--- src/raycasting/camera.cljs
+++ src/raycasting/camera.cljs
@@ -2,5 +2,10 @@
 
 (def camera (atom {:x 0.0 :y 0.0 :degree 0.0}))
 
-; (in-ns 'raycasting.camera)
-; (:degree @camera)
+
+#_(comment
+    (in-ns 'raycasting.camera)
+    (:degree @camera)
+
+    (swap! camera (fn [c] (assoc c :degree 40)))
+    @camera)
