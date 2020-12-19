f32a19510eaba48f9f628a3a31f0e8aa877caef6
Chao-Wei Chen <willhyper@gmail.com>
2020-12-18 23:55:12 -0800

    + (defn draw-line) + (defn draw-camera)

--- src/raycasting/core.cljs
+++ src/raycasting/core.cljs
@@ -2,15 +2,52 @@
   (:require [raycasting.camera :as cam]))
 
 
-(defn start [])
+(defonce ^:dynamic *canvas* nil)
+(defonce ^:dynamic *ctx* nil)
 
 (defn ^:export init []
-  ;; init is called ONCE when the page loads
-  ;; this is called in the index.html and must be exported
-  ;; so it is available even in :advanced release builds
-  (start))
+  (when-let [canvas (. js/document getElementById "raycaster")]
+    (set! *canvas* canvas)
+    (when (. *canvas* -getContext)
+      (set! *ctx* (. *canvas* getContext "2d"))
+      (set! (. *ctx* -fillStyle) "yellow")
+      (. *ctx* fillRect 0 0 512 512))))
 
 (defn stop []
   ;; stop is called before any code is reloaded
   ;; this is controlled by :before-load in the config
   (js/console.log "stop"))
+
+
+(defn draw-line
+  ([[x1 y1] [x2 y2]]
+   (draw-line [x1 y1] [x2 y2] "#000000"))
+  ([[x1 y1] [x2 y2] color]
+   (set! (. *ctx* -strokeStyle) color)
+   (doto *ctx*
+     (.beginPath)
+     (.moveTo x1 y1)
+     (.lineTo x2 y2)
+     (.stroke)))) ; draw it https://www.w3schools.com/tags/canvas_beginpath.asp
+
+
+
+(defn draw-camera
+  "Draws `camera` on canvas, specified by `*ctx*`."
+  [camera]
+  (let [[x y :as pos] [(:x camera) (:y camera)]
+        pointer (cam/move-point pos (:degree camera) 7)
+        width 10
+        color "#000000"]
+    (draw-line pos pointer color)
+    (set! (. *ctx* -fillStyle) color)
+    (. *ctx* fillRect (- x (/ width 2)) (- y (/ width 2)) width width)))
+
+#_(comment
+    (draw-line [0 10] [200 40])
+
+    @cam/camera    
+    (draw-camera @cam/camera)
+
+    )
+
